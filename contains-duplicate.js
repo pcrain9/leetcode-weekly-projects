41e7f97a784038383e90d3a898f2f54663ad4ed5
Perry <80589286+pcrain9@users.noreply.github.com>
2022-01-28 14:37:24 -0600

    problem 2 solved

---
+++ contains-duplicate.js
@@ -0,0 +1,16 @@
+/*week 1, problem 2 from https://techinterviewhandbook.org/best-practice-questions/ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                return true;
+            }
+        }
+    }
+    return false;
+};
